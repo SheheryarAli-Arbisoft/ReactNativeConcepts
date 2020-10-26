0f4b872b93f8f23aee190b614ecce96f6e065f12
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-26 18:43:07 +0500

    Completed the app

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingHorizontal: 20,
   },
   text: {
     fontSize: 30,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    width: '100%',
+    fontSize: 26,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#c0c0c0',
+    padding: 10,
+    marginBottom: 20,
   },
 });
 
 const App = () => {
+  const [textInputValue, setTextInputValue] = useState('Hello world');
+  const [numberInputValue, setNumberInputValue] = useState('123456');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>Simple Text Input</Text>
+      <TextInput
+        value={textInputValue}
+        onChangeText={text => setTextInputValue(text)}
+        style={styles.textInput}
+      />
+
+      <Text style={styles.text}>Numeric Text Input</Text>
+      <TextInput
+        value={numberInputValue}
+        onChangeText={text => setNumberInputValue(text)}
+        keyboardType='numeric'
+        style={styles.textInput}
+      />
+
+      <Text style={styles.text}>Password Text Input</Text>
+      <TextInput secureTextEntry={true} style={styles.textInput} />
     </View>
   );
 };
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React Native Concepts
 
 This repository has been created to maintain a record of all the different Core Component API's in React Native. Each Component API has its own branch and contains a sample project which demonstrates its usage.
+
+## Current branch: TextInput API
+
+The TextInput api renders a native text input field with all the capabilities of a text field such as text type, size, and different styling can also be applied
