a5b4c78ead7b6003d1407a7a6092d1754b5edf20
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-26 18:55:13 +0500

    Completed the app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Button, Linking, Alert, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,15 +7,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 30,
+  button: {
+    marginBottom: 20,
   },
 });
 
 const App = () => {
+  const openLink = url => {
+    Linking.openURL(url).catch(() =>
+      Alert.alert('Error occurred', 'Please try again later')
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <View style={styles.button}>
+        <Button
+          title='Open Link 1'
+          onPress={() => openLink('https://google.com')}
+          style={styles.button}
+        />
+      </View>
+
+      <View style={styles.button}>
+        <Button
+          title='Open Link 2'
+          onPress={() => openLink('fksdjfdslkfjds')}
+          style={styles.button}
+        />
+      </View>
     </View>
   );
 };
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React Native Concepts
 
 This repository has been created to maintain a record of all the different Core Component API's in React Native. Each Component API has its own branch and contains a sample project which demonstrates its usage.
+
+## Current branch: Linking and Alert
+
+The Linking API is used whenever we want want to open a url in the app and it requires the browser to open in order to open the website. The Alert API has also been demonstrated in this branch
