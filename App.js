713290673e9bd63d8aec8dc1946a6f4849eb8ce7
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-27 13:10:06 +0500

    Completed the app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,16 +7,128 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  box1: {
+    width: '100%',
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff0000',
+    borderWidth: 8,
+    borderColor: 'white',
+  },
+  box2: {
+    width: '100%',
+    flexGrow: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 8,
+    borderColor: 'white',
+  },
+  box2Inner1: {
+    height: 100,
+    backgroundColor: '#00bb00',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box2Inner2: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexGrow: 1,
+    backgroundColor: '#006600',
+  },
+  box3: {
+    width: '100%',
+    flexGrow: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 8,
+    borderColor: 'white',
+  },
+  box3Inner1: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+    backgroundColor: '#0000ff',
+  },
+  box3Inner2: {
+    width: '100%',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    flexGrow: 1,
+    flexDirection: 'row',
+  },
+  box3Inner3: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+    backgroundColor: '#000066',
+  },
   text: {
-    fontSize: 30,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box1}>
+        <Text style={styles.text}>Flex Grow: 1</Text>
+      </View>
+      <View style={styles.box2}>
+        <View style={styles.box2Inner1}>
+          <Text style={styles.text}>Height: 100</Text>
+        </View>
+        <View style={styles.box2Inner2}>
+          <Text style={styles.text}>Flex Grow: 1</Text>
+        </View>
+      </View>
+      <View style={styles.box3}>
+        <View style={styles.box3Inner1}>
+          <Text style={styles.text}>Height: 80</Text>
+        </View>
+        <View style={styles.box3Inner2}>
+          <View
+            style={{
+              flexGrow: 1,
+              backgroundColor: '#0000cc',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={styles.text}>FG: 1</Text>
+          </View>
+          <View
+            style={{
+              flexGrow: 1,
+              backgroundColor: '#0000aa',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={styles.text}>FG: 1</Text>
+          </View>
+          <View
+            style={{
+              flexGrow: 1,
+              backgroundColor: '#000077',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text style={styles.text}>FG: 1</Text>
+          </View>
+        </View>
+        <View style={styles.box3Inner3}>
+          <Text style={styles.text}>Height: 80</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React Native Concepts
 
 This repository has been created to maintain a record of all the different Core Component API's in React Native. Each Component API has its own branch and contains a sample project which demonstrates its usage.
+
+## Current branch: Layout API
+
+The default layout used in a React Native application is Flexbox. Any layout is acheived by a combination of flexDirection, alignItems and justifyContent. Along with this other properties of flexbox can also be used such as flexGrow, flexShrink etc. to design a layout according to requirements. The default flexDirection is column.
