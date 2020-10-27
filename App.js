51df2189060562343b7f90d1c13d497d80be1d58
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-27 12:31:10 +0500

    Completed the app

--- App.js
+++ App.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  SectionList,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
-    fontSize: 30,
+  header: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  item: {
+    fontSize: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
   },
 });
 
+const data = [
+  {
+    title: 'Section 1',
+    data: ['Item 1', 'Item 2', 'Item 3'],
+  },
+  {
+    title: 'Section 2',
+    data: ['Item 4', 'Item 5', 'Item 6'],
+  },
+  {
+    title: 'Section 3',
+    data: ['Item 7', 'Item 8', 'Item 9'],
+  },
+];
+
 const App = () => {
+  const renderSectionHeader = ({ section: { title } }) => (
+    <Text style={styles.header}>{title}</Text>
+  );
+
+  const renderItem = ({ item }) => <Text style={styles.item}>{item}</Text>;
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={data}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        keyExtractor={(_, index) => `${index}`}
+      />
+    </SafeAreaView>
   );
 };
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React Native Concepts
 
 This repository has been created to maintain a record of all the different Core Component API's in React Native. Each Component API has its own branch and contains a sample project which demonstrates its usage.
+
+## Current branch: SectionList API
+
+This API works in a similar manner as FlatList but with the added functionality of headings for different sections in a list.
