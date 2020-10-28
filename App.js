22469b010806811e1d2c4e7a6ac331fb3de03124
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-28 16:19:44 +0500

    Added custom icons and completed the app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,6 +18,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World</Text>
+      <Icon name='rocket' size={100} color='#900' />
     </View>
   );
 };
